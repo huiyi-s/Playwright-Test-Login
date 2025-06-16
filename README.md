0edf9d4337ae8fccca77a1f17ca5c85d4fc6d0e5
Hui Yi See <huiyi.see@ibm.com>
2025-06-16 13:44:39 -0300

    Initial commit: Playwright login test with mock HTML page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Playwright login automation with typeScript
+    This project demonstrates UI automation testing of a login flow via Playwright. 
+# Sample test scenario
+- open fake-login.html
+- enter a sample email & password
+- validate password visbility toggle
+- submit the login form 
+# view Fake login page (./fake-login.html)
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,16 @@
 import {test,expect} from '@playwright/test'
 
-test('valid login and toggle password visibility', async ({ page}) => {
-    await page.goto('https://www.facebook.com/login.php/');
+test('valid login and toggle password visibility on local HTML page', async ({ page}) => {
+    await page.goto('file:///C:/Users/HuiYiSee/Playwright/login-automation-ts/fake-login.html');
 
     //fill email and then password
-    await page.fill('//*[@id="email"]','sampleName@gmail.com');
+    await page.getByPlaceholder('Email').fill('sampleName@gmail.com');
     await page.getByPlaceholder('Password').fill('Password@123!');
 
+      
+   // const type = await page.getAttribute('#password-input','type');
+    //expect(type).toBe('text');
+
+    await page.click('#login-button');
 })
+
